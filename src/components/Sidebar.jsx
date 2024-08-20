12b882a4030566656d817dc5c354f58d14df1e5b
Clarinha2124 <clara.silva72@etec.sp.gov.br>
2024-08-20 10:53:18 -0300

    novo

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,18 @@ import styles from './Sidebar.module.css';
 export function Sidebar(){
     return (
 <aside className={styles.Sidebar}>
-lalal
+<img className={styles.cover} src="https://images.unsplash.com/photo-1567674680706-518a0c7fe495?w=300&auto=format&fit=crop&q=40&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDEzfF84ekZIdWhSaHlvfHxlbnwwfHx8fHw%3D" alt="" />
+<div className={styles.profile}>
+    <strong>Clara</strong>
+    <br />
+    <span>
+        Web Developer
+    </span>
+    </div>
+    <footer>
+        <a href="#"> Editar seu perfil</a>
+    </footer>
+
 </aside>
     );
 }
