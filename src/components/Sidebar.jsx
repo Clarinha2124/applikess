5224945a69c21b99594c02516d3ff99a572988c6
Clarinha2124 <clara.silva72@etec.sp.gov.br>
2024-09-03 10:51:59 -0300

    03/09

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,18 +1,31 @@
 import styles from './Sidebar.module.css';
 
+import { PencilLine } from '@phosphor-icons/react/dist/ssr';
+
 export function Sidebar(){
     return (
 <aside className={styles.Sidebar}>
 <img className={styles.cover} src="https://images.unsplash.com/photo-1567674680706-518a0c7fe495?w=300&auto=format&fit=crop&q=40&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDEzfF84ekZIdWhSaHlvfHxlbnwwfHx8fHw%3D" alt="" />
 <div className={styles.profile}>
+<img className={styles.avatar} src="https:github.com/Clarinha2124.png" alt="" />
+
+
+
+
+
+   
     <strong>Clara</strong>
-    <br />
+   
     <span>
         Web Developer
     </span>
+
     </div>
     <footer>
-        <a href="#"> Editar seu perfil</a>
+        <a href="#"> 
+        <PencilLine size={20}/>
+        Editar seu perfil
+        </a>
     </footer>
 
 </aside>
