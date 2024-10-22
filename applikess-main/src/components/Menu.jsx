b9cfd1d3329ec911c8ce5dcf4a095e35acf0441c
Clarinha2124 <clara.silva72@etec.sp.gov.br>
2024-10-22 10:24:41 -0300

    Add files via upload

---
+++ applikess-main/src/components/Menu.jsx
@@ -0,0 +1,17 @@
+<ul id="menu">
+        <li class="home">
+            <a href="INDEX.HTML">Home</a>
+        </li >
+        <li class="sobre">
+            <a href=" sobre.html">Sobre</a>
+        </li>
+        <li class="servios"> 
+            <a href=" servicos.html">Serviços</a>
+        </li>
+        <li class=" noticias">
+            <a href="noticias.html">Notícias</a>
+        </li>
+        <li class="contatos">
+            <a href=" contatos.html">Contatos</a>
+        </li>
+    </ul>
---
+++ applikess-main/src/components/Post.jsx
@@ -0,0 +1,68 @@
+import styles from './Post.module.css';
+import { Comment } from './Comment';
+
+export function Post(){
+    return(
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img 
+                    className={styles.avatar} 
+                    src="https://github.com/Clarinha2124.png"
+                    />
+                    <p></p>
+                    <div className={styles.authorInfo}>
+                        <strong>Clara Cavalheiro</strong>
+                        <span>@clarinhaa</span>
+                    </div>
+                    <p></p>
+                </div>
+
+                <p></p>
+                <time title="10 de setembro às 09:44h" dateTime="2024/09/10 09:44:00">Publicado á 1h</time>
+                <p></p>
+
+            </header>
+
+            <div className={styles.content}>
+                <p>Oioi pessoal!!!</p>
+                <p></p>
+                <p>Mais uma leitura concluida:</p>
+                    <p>Mulherzinhas 5/5!!!</p>
+
+
+               <p></p>
+
+                <img src='/src/assets/mulherzinhas.jpg'></img>
+                <p>
+                    <a href="#">#Amoler</a>{''}
+                    <a href="#">#Recomendo</a>{''}
+                  
+                </p>
+            </div>
+
+            <form className={styles.commentForm}>
+             <strong>Deixe deu feedback</strong>
+
+             <textarea placeholder='deixe um comentário'></textarea>
+
+
+           <footer>
+             <button type='submit'> Publicar </button>
+             </footer>
+            </form>
+
+<div className={styles.commentList}>
+<Comment/>
+<Comment/> 
+<Comment/> 
+</div>
+
+
+
+
+
+        </article>
+
+    )
+}
